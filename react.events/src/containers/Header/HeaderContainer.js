23802577c28eee3a1dc94d29ff7cde79fbaee888
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-13 20:27:49 +0300

    Added space between each item

--- react.events/src/containers/Header/HeaderContainer.js
+++ react.events/src/containers/Header/HeaderContainer.js
@@ -12,12 +12,15 @@ const HeaderContainer = () =>
                     style={{maxHeight: '100px'}}
                     navbarScroll
                 >
-                    <Nav.Link href="#action2"> All Events</Nav.Link>
-                    <Nav.Link href="#action3"> My Events</Nav.Link>
-                    <Nav.Link href="#action4"> About</Nav.Link>
-                    <NavDropdown title="Languages"  id="navbarScrollingDropdown">
+                    <Nav.Link href="#action2" className='px-3'> Events</Nav.Link>
+                    <Nav.Link href="#action3" className='px-3'> Favorite</Nav.Link>
+                    <Nav.Link href="#action4" className='px-3'> About</Nav.Link>
+                    <NavDropdown title="Languages" className='px-3' id="navbarScrollingDropdown">
                         <NavDropdown.Item href="#action5">LT</NavDropdown.Item>
                         <NavDropdown.Item href="#action6">EN</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="ADMIN PANEL"  id="navbarScrollingDropdown">
+                        <Nav.Link href="#action3">All users</Nav.Link>
                     </NavDropdown>
                 </Nav>
                 <Nav.Link href="#" ><i className='fas fa-user'></i> Login</Nav.Link>
