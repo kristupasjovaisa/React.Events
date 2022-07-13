264942129f246f9012cdb75b8e5073a7c81a7992
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-13 19:10:45 +0300

    Implemented footer

--- react.events/src/containers/Footer/FooterContainer.js
+++ react.events/src/containers/Footer/FooterContainer.js
@@ -1,3 +1,15 @@
-const FooterContainer = () => <span>Footer</span>
+import {Col, Container, Row} from "react-bootstrap";
+
+const FooterContainer = () => {
+    return(
+        <footer className='fixed-bottom'>
+            <Container>
+               <Row>
+                   <Col className='text-center py-3'>Copyright &copy; Events</Col>
+               </Row>
+            </Container>
+        </footer>
+    )
+}
 
 export default FooterContainer;
