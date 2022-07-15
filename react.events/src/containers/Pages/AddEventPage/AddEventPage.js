1b5c208f2f27f90d8962f0c7207865caa47470d0
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-15 23:55:56 +0300

    Implemented add event form

--- react.events/src/containers/Pages/AddEventPage/AddEventPage.js
+++ react.events/src/containers/Pages/AddEventPage/AddEventPage.js
@@ -1,3 +1,56 @@
-const AddEventPage = () => <h2>This is new event page</h2>
+import React from "react";
+import {Form} from "react-bootstrap";
+
+const AddEventPage = () => {
+    return (
+        <Form>
+            <Form.Group className='mb-3' controlId='eventName'>
+                <Form.Label>Event name</Form.Label>
+                <Form.Control type='text' placeholder="Enter event name" />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='eventLocation'>
+                <Form.Label>Event location</Form.Label>
+                <Form.Control type='text' placeholder="Enter event location" />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='eventCategory'>
+                <Form.Label>Select category</Form.Label>
+                <Form.Select>
+                    <option></option>
+                    <option>Sports</option>
+                    <option>Concerts</option>
+                    <option>Festivals</option>
+                    <option>Performing Arts</option>
+                    <option>Conferences</option>
+                    <option>Community</option>
+                </Form.Select>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='eventPrice'>
+                <Form.Label>Event price</Form.Label>
+                <Form.Control type='number' placeholder="Enter event price" />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='eventStart'>
+                <Form.Label>Start event date/time</Form.Label>
+                <Form.Control type='text' placeholder="Enter start event date/time" />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='eventEnd'>
+                <Form.Label>End event date/time</Form.Label>
+                <Form.Control type='text' placeholder="Enter end event date time" />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='eventDescription'>
+                <Form.Label>Event description</Form.Label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </Form.Group>
+        </Form>
+    );
+};
+
+// private String name;
+// private String location;
+// private String category;
+// private BigDecimal price;
+// private String startEventDateTime;
+// private String endEventDateTime;
+// private String description;
+
 
 export default AddEventPage;
--- react.events/src/containers/Pages/EventsPage/Events.js
+++ react.events/src/containers/Pages/EventsPage/Events.js
@@ -1,3 +1,34 @@
-const Events = () => <h2>All events page</h2>
+import React, {useState} from "react";
+import {Button, Modal, ModalBody, ModalHeader} from "react-bootstrap";
+import AddEventPage from "../AddEventPage/AddEventPage";
+
+const Events = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <div className='text-center mt-3'>
+            <Modal show={show} onHide={handleClose}>
+                <ModalHeader closeButton>
+                    <strong>Created Your Best Event!</strong>
+                </ModalHeader>
+                <ModalBody>
+                    <AddEventPage/>
+                </ModalBody>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <button className='button-30' role='button' onClick={handleShow}>
+                Add new event
+            </button>
+        </div>
+    );
+};
 
 export default Events;
