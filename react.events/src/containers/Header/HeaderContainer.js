b9e5e523fb7dd7fbc3e8ccf8ef1cfd1f10ccf699
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-15 14:04:19 +0300

    Added Users simple page and Routes

--- react.events/src/containers/Header/HeaderContainer.js
+++ react.events/src/containers/Header/HeaderContainer.js
@@ -4,7 +4,7 @@ import logo from '../logo/logo.svg'
 const HeaderContainer = () =>
     <Navbar bg="light" expand="lg">
         <Container fluid>
-            <Navbar.Brand href="#"> <img src={logo} className='App-logo'/> </Navbar.Brand>
+            <Navbar.Brand href="/"> <img src={logo} className='App-logo'/> </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll"/>
             <Navbar.Collapse id="navbarScroll">
                 <Nav
@@ -12,18 +12,18 @@ const HeaderContainer = () =>
                     style={{maxHeight: '100px'}}
                     navbarScroll
                 >
-                    <Nav.Link href="#action2" className='px-3'> Events</Nav.Link>
-                    <Nav.Link href="#action3" className='px-3'> Favorite</Nav.Link>
-                    <Nav.Link href="#action4" className='px-3'> About</Nav.Link>
+                    <Nav.Link href="/events" className='px-3'> Events</Nav.Link>
+                    <Nav.Link href="/favorite" className='px-3'> Favorite</Nav.Link>
+                    <Nav.Link href="/about" className='px-3'> About</Nav.Link>
                     <NavDropdown title="Languages" className='px-3' id="navbarScrollingDropdown">
                         <NavDropdown.Item href="#action5">LT</NavDropdown.Item>
                         <NavDropdown.Item href="#action6">EN</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="ADMIN PANEL" id="navbarScrollingDropdown">
-                        <Nav.Link href="#action3">All users</Nav.Link>
+                        <Nav.Link href="/users">All users</Nav.Link>
                     </NavDropdown>
                 </Nav>
-                <Nav.Link href="#"><i className='fas fa-user'></i> Login</Nav.Link>
+                <Nav.Link href="/login"><i className='fas fa-user'></i> Login</Nav.Link>
             </Navbar.Collapse>
         </Container>
     </Navbar>
