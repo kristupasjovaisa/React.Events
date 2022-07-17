bbf69ed0d1d7edc3f2c0d052d6464ac8ba20b32b
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-17 17:02:13 +0300

    Fixed card and footer position

--- react.events/src/containers/Footer/FooterContainer.js
+++ react.events/src/containers/Footer/FooterContainer.js
@@ -1,13 +1,11 @@
 import {Col, Container, Row} from "react-bootstrap";
 
 const FooterContainer = () => {
-    return(
-        <footer className='fixed-bottom'>
-            <Container>
-               <Row>
-                   <Col className='text-center py-3'>Copyright &copy; Events</Col>
-               </Row>
-            </Container>
+    return (
+        <footer>
+            <span>
+       Copyright &copy; Events
+    </span>
         </footer>
     )
 }
--- react.events/src/containers/Pages/EventsPage/Events.js
+++ react.events/src/containers/Pages/EventsPage/Events.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Button, Card, Container, Form, ListGroup, Modal, ModalBody, ModalHeader} from "react-bootstrap";
+import {Button, Card, Col, Container, Form, ListGroup, Modal, ModalBody, ModalHeader, Row} from "react-bootstrap";
 import {useSelector, useDispatch} from "react-redux";
 import {addEvent} from "../../../redux/Event/EventsSlice";
 
@@ -107,42 +107,41 @@ const Events = () => {
                     <Card.Title>Add new event</Card.Title>
                 </button>
             </div>
-            <Container>
-                <div className='displayEvents mt-5'>
-                    {eventsList.map((e) => {
-                            return (
-                                <Container>
-                                    <Card style={{width: '18rem'}} className='cardDesign'>
-                                        <Card.Body>
-                                            <Card.Title> Full info about Event</Card.Title>
-                                        </Card.Body>
-                                        <ListGroup className="list-group-flush">
-                                            <ListGroup.Item><Card.Title>Event:</Card.Title> {e.name}</ListGroup.Item>
-                                            <ListGroup.Item><Card.Title>Location:</Card.Title> {e.location}</ListGroup.Item>
-                                            <ListGroup.Item><Card.Title>Category:</Card.Title> {e.category}</ListGroup.Item>
-                                            <ListGroup.Item><Card.Title>Price: </Card.Title>{e.price} $</ListGroup.Item>
-                                            <ListGroup.Item><Card.Title>Start:</Card.Title> {e.startEventDateTime}
-                                            </ListGroup.Item>
-                                            <ListGroup.Item><Card.Title>End: </Card.Title>{e.endEventDateTime}
-                                            </ListGroup.Item>
-                                        </ListGroup>
-                                        <Card.Body>
-                                            <Card.Text>
-                                                {e.description}
-                                            </Card.Text>
-                                        </Card.Body>
-                                        <Card.Body>
-                                            <Button variant="outline-warning" size='sm'>Update</Button>{' '}
-                                            <Button variant="outline-success" size='sm'>Favorite</Button>{' '}
-                                            <Button variant="outline-danger" size='sm'>Delete</Button>{' '}
-                                        </Card.Body>
-                                    </Card>
-                                </Container>
-                            )
-                        }
-                    )}
-                </div>
-            </Container>
+            <div className='displayEvents mt-5 '>
+                {eventsList.map((e) => {
+                        return (
+                            <Card style={{width: '18rem'}} className='cardDesign'>
+                                <Card.Body>
+                                    <Card.Title> Full info about Event</Card.Title>
+                                </Card.Body>
+                                <ListGroup className="list-group-flush">
+                                    <ListGroup.Item><Card.Title>Event:</Card.Title> {e.name}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item><Card.Title>Location:</Card.Title> {e.location}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item><Card.Title>Category:</Card.Title> {e.category}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item><Card.Title>Price: </Card.Title>{e.price} $</ListGroup.Item>
+                                    <ListGroup.Item><Card.Title>Start:</Card.Title> {e.startEventDateTime}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item><Card.Title>End: </Card.Title>{e.endEventDateTime}
+                                    </ListGroup.Item>
+                                </ListGroup>
+                                <Card.Body>
+                                    <Card.Text>
+                                        {e.description}
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Body>
+                                    <Button variant="outline-warning" size='sm'>Update</Button>{' '}
+                                    <Button variant="outline-success" size='sm'>Favorite</Button>{' '}
+                                    <Button variant="outline-danger" size='sm'>Delete</Button>{' '}
+                                </Card.Body>
+                            </Card>
+                        )
+                    }
+                )}
+            </div>
         </>
     );
 };
