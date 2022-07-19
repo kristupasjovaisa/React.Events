0d17ad3427d9aa0e0323bb4269e692d471d1877b
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-19 16:54:31 +0300

    Added UserFakeData

---
+++ react.events/src/containers/FakeData/UserFakeData.js
@@ -0,0 +1,45 @@
+export const UserFakeData = [
+    {
+        id:1,
+        nickname: 'Antanas',
+        city: 'Krekenava',
+        email: 'antanas@gmail.com',
+        phoneNumber: '8612345678'
+    },
+
+    {
+        id:2,
+        nickname: 'Antanas',
+        city: 'Krekenava',
+        email: 'antanas@gmail.com',
+        phoneNumber: '8612345678'
+    },
+    {
+        id:3,
+        nickname: 'Antanas',
+        city: 'Krekenava',
+        email: 'antanas@gmail.com',
+        phoneNumber: '8612345678'
+    },
+    {
+        id:4,
+        nickname: 'Antanas',
+        city: 'Krekenava',
+        email: 'antanas@gmail.com',
+        phoneNumber: '8612345678'
+    },
+    {
+        id:5,
+        nickname: 'Antanas',
+        city: 'Krekenava',
+        email: 'antanas@gmail.com',
+        phoneNumber: '8612345678'
+    },
+    {
+        id:6,
+        nickname: 'Antanas',
+        city: 'Krekenava',
+        email: 'antanas@gmail.com',
+        phoneNumber: '8612345678'
+    },
+];
