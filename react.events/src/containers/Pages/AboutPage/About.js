695629034d2ea98a6447f29d9a7878d79daee6fa
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-19 18:24:43 +0300

    Implemented about page

--- react.events/src/containers/Pages/AboutPage/About.js
+++ react.events/src/containers/Pages/AboutPage/About.js
@@ -1,3 +1,29 @@
-const About = () => <h2>This is about page</h2>
+const About = () => {
+    return (
+        <>
+            <div>
+                <h3 className='text about mt-4'>About us</h3>
+                <p className='text mt-4'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias blanditiis consequuntur corporis
+                    cum eveniet excepturi explicabo laudantium minima mollitia, necessitatibus nisi obcaecati
+                    praesentium quasi quod ratione tempora veritatis voluptas.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloribus ea in
+                    maiores obcaecati saepe sunt! Id, quae, quibusdam. Laboriosam minima nihil omnis quam sequi soluta!
+                    Atque, nemo.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias blanditiis consequuntur corporis
+                    cum eveniet excepturi explicabo laudantium minima mollitia, necessitatibus nisi obcaecati
+                    praesentium quasi quod ratione tempora veritatis voluptas.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloribus ea in
+                    maiores obcaecati saepe sunt! Id, quae, quibusdam. Laboriosam minima nihil omnis quam sequi soluta!
+                    Atque, nemo.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias blanditiis consequuntur corporis
+                    cum eveniet excepturi explicabo laudantium minima mollitia, necessitatibus nisi obcaecati
+                    praesentium quasi quod ratione tempora veritatis voluptas.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloribus ea in
+                    maiores obcaecati saepe sunt! Id, quae, quibusdam. Laboriosam minima nihil omnis quam sequi soluta!
+                    Atque, nemo.
+                </p>
+            </div>
+        </>
+    );
+};
 
 export default About;
