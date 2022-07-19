b203e46423b4dad6b426393ed97131c320d850d5
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-19 19:05:54 +0300

    Created register form

--- react.events/src/containers/Pages/RegisterPage/Register.js
+++ react.events/src/containers/Pages/RegisterPage/Register.js
@@ -1,3 +1,34 @@
-const Register = () => <h2>This register</h2>
+import {Button, Form} from "react-bootstrap";
+
+const Register = () => {
+    return(
+        <>
+            <Form>
+                <Form.Label><h3>Sign Up</h3></Form.Label>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Nickname</Form.Label>
+                    <Form.Control type="text" placeholder="Enter nickname" />
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                    <Form.Label>City</Form.Label>
+                    <Form.Control type="text" placeholder="Enter city" />
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Phone number</Form.Label>
+                    <Form.Control type="text" placeholder="Enter phone number" />
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="outline-success" size='sm'>Submit</Button>
+            </Form>
+        </>
+    )
+}
 
 export default Register;
