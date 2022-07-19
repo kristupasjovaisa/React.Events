7e565a1b12c4b901bf03a801b64aa48c971a1725
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-19 17:04:45 +0300

    Deleted redundat files

--- react.events/src/redux/User/UserSlice.js
+++ react.events/src/redux/User/UserSlice.js
@@ -4,12 +4,6 @@ import {UserFakeData} from "../../containers/FakeData/UserFakeData";
 export const userSlice = createSlice({
     name:'users',
     initialState:{value:UserFakeData},
-    reducers:{
-        addUser:(state,action) =>{
-            // state.value.push(action.payload)
-        },
-    },
 });
 
-// export const {addUser} = userSlice.actions
 export default userSlice.reducer;
