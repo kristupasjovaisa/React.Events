7fb6d712e656c825c797b23e3ef1151c0b9627ed
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-19 23:12:39 +0300

    Implemented register form with validation

--- react.events/src/containers/Pages/RegisterPage/Register.js
+++ react.events/src/containers/Pages/RegisterPage/Register.js
@@ -1,34 +1,76 @@
-import {Button, Form} from "react-bootstrap";
+import {useForm} from 'react-hook-form'
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from 'yup'
 
 const Register = () => {
-    return(
-        <>
-            <Form>
-                <Form.Label><h3>Sign Up</h3></Form.Label>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Nickname</Form.Label>
-                    <Form.Control type="text" placeholder="Enter nickname" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>City</Form.Label>
-                    <Form.Control type="text" placeholder="Enter city" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Phone number</Form.Label>
-                    <Form.Control type="text" placeholder="Enter phone number" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="outline-success" size='sm'>Submit</Button>
-            </Form>
-        </>
+
+    const schema = yup.object().shape({
+        nickname: yup.string().required(),
+        email: yup.string().email().required(),
+        city: yup.string().required(),
+        phoneNumber: yup.string().min(9).max(9).required(),
+        password: yup.string().min(5).max(10).required()
+    })
+
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema),
+        mode:'onTouched'
+    });
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+    return (
+        <div className='registerForm'>
+            <h4 className='text-center'>Sign Up</h4>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label>Nickname</label>
+                    <input type='text' className='form-control font-weight-bolt'
+                           placeholder='Your nickname' {...register('nickname')}/>
+                    {errors.nickname?.type === 'required' &&
+                        <p className='text-bg-light'> Nickname is required</p>}
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type='text' className='form-control font-weight-bolt'
+                           placeholder='Your email' {...register('email')}/>
+                    {errors.email?.type === 'required' &&
+                        <p className='text-bg-light'> Email is required</p>}
+                    {errors.email?.type === 'email' &&
+                        <p className='text-bg-light'> Enter a valid email (ex. email@gmail.com)</p>}
+                </div>
+                <div>
+                    <label>City</label>
+                    <input type='text' className='form-control font-weight-bolt'
+                           placeholder='Your city' {...register('city')}/>
+                    {errors.city?.type === 'required' &&
+                        <p className='text-bg-light'> City is required</p>}
+                </div>
+                <div>
+                    <label>Phone number</label>
+                    <input type='text' className='form-control font-weight-bolt'
+                           placeholder='Your phone number' {...register('phoneNumber')}/>
+                    {errors.phoneNumber?.type === 'required' &&
+                        <p className='text-bg-light'> Phone number is required</p>}
+                    {errors.phoneNumber?.type === 'min' &&
+                        <p className='text-bg-light'> The phone number must be 9 digits long </p>}
+                    {errors.phoneNumber?.type === 'max' &&
+                        <p className='text-bg-light'> The phone number must be 9 digits long </p>}
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type='password' className='form-control font-weight-bolt'
+                           placeholder='Your password'{...register('password')}/>
+                    {errors.password?.type === 'required' &&
+                        <p className='text-bg-light'> Password is required</p>}
+                    {errors.password?.type === 'min' &&
+                        <p className='text-bg-light'> Password length must be at least 6</p>}
+                </div>
+                <button className='button-30 mt-4' role='button'> Sign up</button>
+            </form>
+        </div>
     )
 }
 
 export default Register;
+
