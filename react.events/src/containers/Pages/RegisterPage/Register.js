3607bcb67166783238e29cc2791f5499891f1caa
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-21 00:21:08 +0300

    Fixed register form

--- react.events/src/containers/Pages/RegisterPage/Register.js
+++ react.events/src/containers/Pages/RegisterPage/Register.js
@@ -23,33 +23,29 @@ const Register = () => {
         <div className='registerForm'>
             <h4 className='text-center'>Sign Up</h4>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label>Nickname</label>
+                <div className='mt-4'>
                     <input type='text' className='form-control font-weight-bolt'
-                           placeholder='Your nickname' {...register('nickname')}/>
+                           placeholder='Nickname' {...register('nickname')}/>
                     {errors.nickname?.type === 'required' &&
                         <p className='text-bg-light'> Nickname is required</p>}
                 </div>
-                <div>
-                    <label>Email</label>
+                <div className='mt-2'>
                     <input type='text' className='form-control font-weight-bolt'
-                           placeholder='Your email' {...register('email')}/>
+                           placeholder='Email' {...register('email')}/>
                     {errors.email?.type === 'required' &&
                         <p className='text-bg-light'> Email is required</p>}
                     {errors.email?.type === 'email' &&
                         <p className='text-bg-light'> Enter a valid email (ex. email@gmail.com)</p>}
                 </div>
-                <div>
-                    <label>City</label>
+                <div className='mt-2'>
                     <input type='text' className='form-control font-weight-bolt'
-                           placeholder='Your city' {...register('city')}/>
+                           placeholder='City' {...register('city')}/>
                     {errors.city?.type === 'required' &&
                         <p className='text-bg-light'> City is required</p>}
                 </div>
-                <div>
-                    <label>Phone number</label>
+                <div className='mt-2'>
                     <input type='text' className='form-control font-weight-bolt'
-                           placeholder='Your phone number' {...register('phoneNumber')}/>
+                           placeholder='Phone number' {...register('phoneNumber')}/>
                     {errors.phoneNumber?.type === 'required' &&
                         <p className='text-bg-light'> Phone number is required</p>}
                     {errors.phoneNumber?.type === 'min' &&
@@ -57,16 +53,15 @@ const Register = () => {
                     {errors.phoneNumber?.type === 'max' &&
                         <p className='text-bg-light'> The phone number must be 9 digits long </p>}
                 </div>
-                <div>
-                    <label>Password</label>
+                <div className='mt-2'>
                     <input type='password' className='form-control font-weight-bolt'
-                           placeholder='Your password'{...register('password')}/>
+                           placeholder='Password'{...register('password')}/>
                     {errors.password?.type === 'required' &&
                         <p className='text-bg-light'> Password is required</p>}
                     {errors.password?.type === 'min' &&
                         <p className='text-bg-light'> Password length must be at least 6</p>}
                 </div>
-                <button className='button-30 mt-4 bolt' role='button'> Sign up</button>
+                <button className='button-30-1 mt-4' role='button'> Sign up</button>
             </form>
         </div>
     )
