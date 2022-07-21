a5e2aa1c4d02343ef63e863e542520f7fd1789d1
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-22 01:05:29 +0300

    Translated user account page

--- react.events/src/containers/Pages/UserAccount/UserAccount.js
+++ react.events/src/containers/Pages/UserAccount/UserAccount.js
@@ -1,30 +1,33 @@
 import { Card, ListGroup} from "react-bootstrap";
 import EventList from "../../../components/EventList/EventList";
+import {useTranslation} from "react-i18next";
 
 const UserAccount = () => {
+    const {t}=useTranslation();
 
     return (
         <>
             <div className='mainUser mt-4'>
                 <Card className='cardUser'>
-                    <Card.Header as="h5">My Profile</Card.Header>
+                    <Card.Header as="h5">{t('My Profile')}</Card.Header>
                     <Card.Body>
                         <ListGroup className="list-group-flush">
-                            <ListGroup.Item><Card.Title>Nickname:</Card.Title> {'Petras'}
+                            <ListGroup.Item><Card.Title>{t('Nickname')}:</Card.Title> {'Petras'}
                             </ListGroup.Item>
-                            <ListGroup.Item><Card.Title>City:</Card.Title> {'Kaunas'}
+                            <ListGroup.Item><Card.Title>{t('City')}:</Card.Title> {'Kaunas'}
                             </ListGroup.Item>
-                            <ListGroup.Item><Card.Title>Email:</Card.Title> {'petras@gmail.com'}
+                            <ListGroup.Item><Card.Title>{t('Email')}:</Card.Title> {'petras@gmail.com'}
                             </ListGroup.Item>
-                            <ListGroup.Item><Card.Title>Phone number:</Card.Title> {'8652222325'}
+                            <ListGroup.Item><Card.Title>{t('Phone number')}:</Card.Title> {'8652222325'}
                             </ListGroup.Item>
-                            <ListGroup.Item><Card.Title>My Events:</Card.Title> {'8'}
+                            <ListGroup.Item><Card.Title>{t('My Events')}:</Card.Title> {'8'}
                             </ListGroup.Item>
                         </ListGroup>
                     </Card.Body>
                 </Card>
             </div>
             <div className='accountLine'></div>
+            <h3 className='text about mt-4'>{t('My Events')}</h3>
             <EventList/>
         </>
     );
