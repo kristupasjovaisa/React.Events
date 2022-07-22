3be6bb9b5f228f3266f90c557e59067cdb5f3e1c
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-22 21:04:37 +0300

    Deleted redundat files

--- react.events/src/components/EventList/EventList.js
+++ react.events/src/components/EventList/EventList.js
@@ -1,45 +1,36 @@
-import {Button, Card,ListGroup} from "react-bootstrap";
-import {useSelector} from "react-redux";
+import {Button, Card, ListGroup} from "react-bootstrap";
 import {useTranslation} from "react-i18next";
 
-const EventList = () =>{
-    const {t}=useTranslation();
-    const eventsList = useSelector((state) => state.events.value)
+const EventList = () => {
+    const {t} = useTranslation();
     return (
         <>
             <div className='displayEvents mt-5 '>
-                {eventsList.map((e) => {
-                        return (
-                            <Card style={{width: '18rem'}} className='cardDesign'>
-                                <Card.Body>
-                                    <Card.Title> {t('Full info about Event')}</Card.Title>
-                                </Card.Body>
-                                <ListGroup className="list-group-flush">
-                                    <ListGroup.Item><Card.Title>{t('Event name')}:</Card.Title> {e.name}
-                                    </ListGroup.Item>
-                                    <ListGroup.Item><Card.Title>{t('Location')}:</Card.Title> {e.location}
-                                    </ListGroup.Item>
-                                    <ListGroup.Item><Card.Title>{t('Category')}:</Card.Title> {e.category}
-                                    </ListGroup.Item>
-                                    <ListGroup.Item><Card.Title>{t('Price')}: </Card.Title>{e.price} $</ListGroup.Item>
-                                    <ListGroup.Item><Card.Title>{t('Start')}:</Card.Title> {e.startEventDateTime}
-                                    </ListGroup.Item>
-                                    <ListGroup.Item><Card.Title>{t('End')}: </Card.Title>{e.endEventDateTime}
-                                    </ListGroup.Item>
-                                </ListGroup>
-                                <Card.Body>
-                                    <Card.Text>
-                                        {e.description}
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Body>
-                                    <Button variant="outline-warning" size='sm'>{t('Update')}</Button>{' '}
-                                    <Button variant="outline-danger" size='sm'>{t('Delete')}</Button>{' '}
-                                </Card.Body>
-                            </Card>
-                        )
-                    }
-                )}
+                <Card style={{width: '18rem'}} className='cardDesign'>
+                    <Card.Body>
+                        <Card.Title> {t('Full info about Event')}</Card.Title>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroup.Item><Card.Title>{t('Event name')}:</Card.Title>
+                        </ListGroup.Item>
+                        <ListGroup.Item><Card.Title>{t('Location')}:</Card.Title>
+                        </ListGroup.Item>
+                        <ListGroup.Item><Card.Title>{t('Category')}:</Card.Title>
+                        </ListGroup.Item>
+                        <ListGroup.Item><Card.Title>{t('Price')}: </Card.Title></ListGroup.Item>
+                        <ListGroup.Item><Card.Title>{t('Start')}:</Card.Title>
+                        </ListGroup.Item>
+                        <ListGroup.Item><Card.Title>{t('End')}: </Card.Title>
+                        </ListGroup.Item>
+                    </ListGroup>
+                    <Card.Body>
+                        <Card.Text></Card.Text>
+                    </Card.Body>
+                    <Card.Body>
+                        <Button variant="outline-warning" size='sm'>{t('Update')}</Button>{' '}
+                        <Button variant="outline-danger" size='sm'>{t('Delete')}</Button>{' '}
+                    </Card.Body>
+                </Card>
             </div>
         </>
     );
